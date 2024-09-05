41acece2e9946a0090568a406cfe3d196a3ff58a
BOSSNEX <quiverabraham@gmail.com>
2024-09-06 03:06:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def load_player_data(cpm):
     
 def load_key_data(cpm):
     data = cpm.get_key_data()
-    console.print("[bold][red]<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>[/red][/bold]")
+    console.print("[bold][red]<<<<<<<<<<<<<<<<<<<<<[/red][ KEYS ][red]>>>>>>>>>>>>>>>>>>>>>[/red][/bold]")
     console.print(f"[bold green]Access Key [/bold green]: { data.get('access_key') }.")
     console.print(f"[bold green]Telegram ID[/bold green]: { data.get('telegram_id') }.")
     console.print(f"[bold green]Balance    [/bold green]: { (data.get('coins') if not data.get('is_unlimited') else 'Unlimited') }.")
