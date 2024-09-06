54127c4fea77aab710359cf34ddddddd3f339256
BOSSNEX <quiverabraham@gmail.com>
2024-09-06 15:32:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def load_key_data(cpm):
     console.print("[bold][red]<<<<<<<<<<<<<<<<<<<<<[/red][ KEYS ][red]>>>>>>>>>>>>>>>>>>>>>[/red][/bold]")
     console.print(f"[bold green]Access Key [/bold green]: { data.get('access_key') }.")
     console.print(f"[bold green]Telegram ID[/bold green]: { data.get('telegram_id') }.")
-    console.print(f"[bold green]Balance    [/bold green]: { (data.get('coins') if not data.get('is_Unlimited') else 'Lifetime') }.")
+    console.print(f"[bold green]Balance    [/bold green]: { (data.get('coins') if not data.get('is_unlimited') else 'Unlimited') }.")
 
 def prompt_valid_value(content, tag, password=False):
     while True:
